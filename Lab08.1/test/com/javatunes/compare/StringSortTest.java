8cc8de02c24a61c05b92a3c0cca5d190400314c7
sonp1998 <sonp1998@gmail.com>
2022-09-28 16:17:10 -0400

    Completed Lab 8.2.

--- Lab08.1/test/com/javatunes/compare/StringSortTest.java
+++ Lab08.1/test/com/javatunes/compare/StringSortTest.java
@@ -33,5 +33,10 @@ public class StringSortTest {
     // TODO: call names.sort() and pass in an "expression" lambda
     // hint: a compatible lambda will have this form:
     //       (obj1, obj2) -> expression-that-evaluates-to-int
+
+    names.sort((str1, str2) -> str1.length() - str2.length() );
+    System.out.println(names);
   }
 }
+
+   // names.sort(Comparator.comparingInt(String::length));
